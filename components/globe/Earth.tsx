0326e77d87b77a140f331f89702fef4109812698
SRUSHTI NB <srushtinb08@gmail.com>
2026-02-18 14:10:14 +0530

    Update Earth.tsx

--- components/globe/Earth.tsx
+++ components/globe/Earth.tsx
@@ -1,40 +1,4 @@
-// "use client";
 
-// import { useRef } from "react";
-// import { useFrame, useLoader } from "@react-three/fiber";
-// import { TextureLoader } from "three";
-// import * as THREE from "three";
-
-// export default function Earth() {
-//   const earthRef = useRef<THREE.Mesh>(null!);
-
-//   // Load textures
-//   const dayMap = useLoader(TextureLoader, "/textures/earth_day.jpg");
-//   const nightMap = useLoader(TextureLoader, "/textures/earth_night.jpg");
-
-//   // INDIA CENTER (78°E ≈ 1.36 rad)
-//   const indiaRotation = -1.36;
-
-//   useFrame(() => {
-//     if (earthRef.current) {
-//       earthRef.current.rotation.y += 0.0004; // slow natural rotation
-//     }
-//   });
-
-//   return (
-//     <mesh ref={earthRef} rotation={[0, indiaRotation, 0]}>
-//       <sphereGeometry args={[1.5, 128, 128]} />
-//       <meshStandardMaterial
-//         map={dayMap}
-//         emissiveMap={nightMap}
-//         emissive={"#ffffff"}
-//         emissiveIntensity={0.6}
-//         roughness={0.7}
-//         metalness={0.1}
-//       />
-//     </mesh>
-//   );
-// }
 import { useRef } from "react";
 import { useFrame, useLoader, useThree } from "@react-three/fiber";
 import { TextureLoader } from "three";
