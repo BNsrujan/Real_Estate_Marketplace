9dc4227659894dd08e8ef1aed804333da4b8421b
BNsrujan <srujanbn12@gmail.com>
2026-02-22 23:48:53 +0530

    creating a togle button

---
+++ components/toglebuttons.tsx
@@ -0,0 +1,15 @@
+export default function ToggleButton(){
+    return (
+        <div>
+            <div>
+            land
+            </div>
+            <div>
+                house
+            </div>
+            <div>
+                site
+            </div>
+        </div>
+    )
+}
