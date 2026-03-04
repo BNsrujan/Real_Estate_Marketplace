900e287a6ca0ca4c0091a301dd86b831821eab81
srushtinb <srushtinb08@gmail.com>
2026-03-04 09:30:35 +0530

    MapLibre globe

--- app/page.tsx
+++ app/page.tsx
@@ -1,51 +1,5 @@
-// import LandingPage from "@/pages/landing.page";
-
-// export default function Home() {
-//   return (
-//     <main className="flex items-center justify-center h-screen bg-black text-white">
-//       <LandingPage/>
-//     </main>
-//   );
-// }
-"use client"
-import { useEffect } from 'react';
+import GlobeView from "../components/globe/GlobeView";
 
 export default function Home() {
-  useEffect(() => {
-    
-    const script = document.createElement('script');
-    script.src = 'https://api.kmzview.com/js/KMZView.js';
-    script.async = true;
-    script.onload = () => {
-      if (window.KMZView) {
-        window.KMZView.init(document.getElementById('kmzv-container'), {
-          LeftSideControlsTop: '80px',
-          LeftSideControlsBottom: '40px',
-          RightSideControlsTop: '14px',
-          RightSideControlsBottom: '200px',
-          ShowMapControl: false,
-          FillRate: 0.1,
-          MapOpts: {
-            bounds: [
-              [-180, -90],
-              [180, 90]
-            ],
-            antialias: true,
-            fadeDuration: 0,
-            logoPosition: 'bottom-left',
-            performanceMetricsCollection: false,
-            attributionControl: false
-          }
-        });
-      }
-    };
-    document.body.appendChild(script);
-
-    return () => {
-      
-    };
-  }, []);
-
-  return <div id="kmzv-container"></div>;
+  return <GlobeView />;
 }
-
